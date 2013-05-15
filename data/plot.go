1485821c82c5619deb1826a24ffba2f7ff27e22b
Ryan Fitzgerald <rwfitzge@gmail.com>
2013-05-14 23:46:45 -0700

    Factor plot reading out into separate object

--- data/plot.go
+++ data/plot.go
@@ -18,21 +18,39 @@ type Plot struct {
 	UploadedAt time.Time
 }
 
+type plotFileReader struct {
+	*bufio.Reader
+	values int
+	errors int
+}
+
 func Parse(name string, file io.Reader) (*Plot, error) {
 	plot := &Plot{"", name, []float64{}, time.Now()}
 	hash := sha256.New()
 	pipe := bufio.NewReader(io.TeeReader(file, hash))
-
-	values := 0
-	errors := 0
+	parser := &plotFileReader{pipe, 0, 0}
 
 	for {
-		line, err := pipe.ReadString('\n')
+		value, err := parser.readFloat()
 		if err == io.EOF {
 			break
 		}
 		if err != nil {
 			return nil, err
+		}
+		plot.Data = append(plot.Data, value)
+	}
+
+	plot.Uid = hashUid(hash)
+
+	return plot, nil
+}
+
+func (self plotFileReader) readFloat() (float64, error) {
+	for {
+		line, err := self.ReadString('\n')
+		if err != nil {
+			return 0, err
 		}
 
 		line = strings.TrimSpace(line)
@@ -43,22 +61,19 @@ func Parse(name string, file io.Reader) (*Plot, error) {
 		value, parseErr := strconv.ParseFloat(line, 64)
 
 		if parseErr == nil {
-			plot.Data = append(plot.Data, value)
-			values += 1
-			if values > 1000*1000 {
-				return nil, fmt.Errorf("Too many values in file (max is 1,000,000)")
+			self.values += 1
+			if self.values > 1000*1000 {
+				return 0, fmt.Errorf("Too many values in file (max is 1,000,000)")
 			}
+			return value, nil
 		} else {
-			errors += 1
-			if errors > 1000 {
-				return nil, fmt.Errorf("File too messy to read. Prefix lines with # to make them comments.")
+			self.errors += 1
+			if self.errors > 1000 {
+				return 0, fmt.Errorf("File too messy to read. Prefix lines with # to make them comments.")
 			}
 		}
 	}
-
-	plot.Uid = hashUid(hash)
-
-	return plot, nil
+	panic("remove this in Go 1.1")
 }
 
 func hashUid(h hash.Hash) string {
