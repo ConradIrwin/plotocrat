29938402143275ee0dd744db39703b942e458574
Conrad Irwin <conrad.irwin@gmail.com>
2013-05-17 23:55:29 -0700

    gofmt

--- plotocrat.go
+++ plotocrat.go
@@ -1,19 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"github.com/ConradIrwin/plotocrat/db"
 	"github.com/ConradIrwin/plotocrat/http"
 	"os"
-    "fmt"
 )
 
 func main() {
 	db.Setup(os.Getenv("MONGOHQ_URL"))
 
-    port := os.Getenv("PORT")
-    if port == "" {
-        port = "5000"
-    }
-    fmt.Println("plotocrat listening on localhost:", port)
-    http.Listen(port)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "5000"
+	}
+	fmt.Println("plotocrat listening on localhost:", port)
+	http.Listen(port)
 }
